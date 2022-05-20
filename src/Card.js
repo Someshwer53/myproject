2ffcce3d1ce1ffd9c9eb9f25f24a8432ccd7695c
Someshwer Singh <someshwers53@gmail.com>
2022-05-19 19:01:20 -0700

    navabr and card

--- src/Card.js
+++ src/Card.js
@@ -2,22 +2,25 @@ import React  from "react";
 import ract from "../src/Rectangle.png";
 import pics from "../src/Oval.png";
 import background from "../src/Mask.png";
+import Ract from "../src/Rect.png";
+import Ractlen from "../src/Rectlen.png";
+import RectNeuro from "../src/Rectangleneuro.png";
 import './Card.css';
  const Card=()=>{
     return(
         <>
-          <div className='ovel_image'>
-           <img src={pics} height="190" width="190"/>
+          <div className='oval_image'>
+           <img src={pics} height="190" width="190" alt="oval"/>
            </div>
 
 
            <div className="background-img">
-               <img src={background} height="953" width="1491"></img>
+               <img src={background} height="953" width="1491" alt="background"></img>
            </div>
 
 
          <div className="card-img">
-         <img src={ract}  height="300" width="300"/>
+         <img src={ract}  height="300" width="300" alt="card"/>
          </div>
           
          <div className="card-text">
@@ -28,10 +31,33 @@ import './Card.css';
             treatments and supportive
             care services in a single convenient
              location.
-        </p>
+        </p>  
         <button className="btn btn-primary">Make An Appointment</button>
         <button className="btn btn-light button1">Departments</button>
          </div>
+
+
+         <div className="service_card">
+           <h2>Our Services</h2>  
+          <p>We provide tha most full medical services, so every person could
+              heave tha oppurtunity to receive quality medical help.</p> 
+            <img src={Ract} height="100" width="100" alt="ract"/> 
+            <h5 className="text">Dental Care</h5>
+         </div>
+
+      
+
+          <div>
+          <img src={Ractlen} height="300" width="300" alt="reactlen"/>  
+         <h5 className="text_len">Pulmonary</h5>
+          </div>
+        
+ 
+         <div className="neuro_img">
+           <img src={RectNeuro} height="200" width="200" alt="neuro"/>
+         </div>
+        
+        
         </>
     )
  }
