10c603c1b597ba9e7deb491ecd2ca9a25ee68442
Someshwer Singh <someshwers53@gmail.com>
2022-05-21 01:57:51 -0700

    Navbar and Service

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import pic from "../MediCare+.jpg";
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 const Navbar = () => {
   return (
     <>
-        <nav className="navbar navbar-expand-lg  fixed-top">
+        <nav className="navbar navbar-expand-lg">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-       <img src={pic} height="30" width="30" className="img"/>
-      </a>
+      <Link className="navbar-brand" to="">
+       <img src={pic} height="30" width="30" className="img" alt="mylogo"/>
+      </Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link" aria-current="page" href="/">Home</a>
+            <Link className="nav-link" aria-current="page" to="/home">Home</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link "  href="/">About</a>
+            <Link className="nav-link" to="/about">About</Link>
           </li>
   
          <li className="nav-item">
-           <a className='nav-link' href="/">Services</a>
+           <Link className="nav-link" to="/services">Services</Link>
          </li>
 
         <li className="nav-item">
-          <a className="nav-link"  href="/">News</a>
+          <Link className="nav-link"  to="/news">News</Link>
         </li>
 
        <li className="nav-item">
-        <a> <button className="btn btn-primary me-2" type="button">Contact</button></a>
+        <Link><button className="btn btn-primary me-2" type="button">Contact</button></Link>
        </li>
         </ul>
         
